4feb78965ae38505e782424526d9b99f2696c7b9
tobi2011 <jcheneuer2011@gmail.com>
2019-05-20 19:10:38 -0400

    app.py update

--- Code/app.py
+++ Code/app.py
@@ -10,17 +10,9 @@ from sqlalchemy import create_engine
 
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db.sqlite"
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '')
-db = SQLAlchemy(app)
-
-# from .models import Pet
 
 
-# create route that renders index.html template
-@app.route("/")
-def home():
-    return render_template("index.html")
+
 
 
 #################################################
